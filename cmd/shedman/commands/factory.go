d63b152666596783e02deea7c12fbcfbdef7f18d
Shedrack Ajaegbu <shedrack3@gmail.com>
2026-01-12 05:12:02 +0100

    chore(cleanup): remove monologue comments from factory

--- cmd/shedman/commands/factory.go
+++ cmd/shedman/commands/factory.go
@@ -14,30 +14,11 @@ import (
 // NewEngineWithConfig creates an Engine with backends auto-detected from config.
 func NewEngineWithConfig(cfg *config.Config) (*core.Engine, error) {
 	e := core.NewEngine()
-	// Need to set config manually or add SetConfig to engine?
-	// Engine struct has config field but NewEngine doesn't take it.
-	// Since we are moving logic, we might need a setter or modified constructor in core.
-	// For now, let's assume we can modify it or access it if exported.
-	// Wait, Engine struct fields are private (lowercase backends/config).
-	// But NewEngine returns *Engine.
-	// We need a way to set config.
-	// Let's check Engine API.
-
-	// Assuming core.DetectBackendWithConfig existed? No, it was removed.
-	// We implement detection here.
 
 	officialBackend, err := DetectBackendWithConfig(&cfg.Backend)
 	if err == nil && officialBackend != nil {
 		e.SetOfficialBackend(officialBackend)
 	}
-
-	// We cannot set e.config directly if private.
-	// core/engine.go has GetConfig(). Does it have SetConfig?
-	// If not, we should adding it or passing it to NewEngine.
-	// Checking previous engine.go content: NewEngine() didn't take config.
-	// NewEngineWithConfig() did set it.
-	// So we need to add SetConfig to Engine in a separate step or modify NewEngine signature.
-	// Let's add SetConfig to Engine first.
 
 	return e, nil
 }
